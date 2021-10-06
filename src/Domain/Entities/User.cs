5d473ce36d74440ee2d250b062a33d4fc4844771
bogdan_yatsiv <bogdanyatsiv@gmail.com>
2021-10-06 15:57:46 +0300

    add User entity

---
+++ src/Domain/Entities/User.cs
@@ -0,0 +1,33 @@
+﻿using BvAcademyPortal.Domain.Common;
+using BvAcademyPortal.Domain.Enums;
+using BvAcademyPortal.Domain.Events;
+using System;
+using System.Collections.Generic;
+
+namespace BvAcademyPortal.Domain.Entities
+{
+    class User : AuditableEntity
+    {
+        public int Id { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public string City { get; set; }
+
+        public string Email { get; set; }
+
+        public string SkypeName { get; set; }
+
+        public string EducationalEstablishment { get; set; }
+
+        public string Faculty { get; set; }
+
+        public string EnglishLevel { get; set; }
+    }
+}
