f099f40938f75cc4c8af9cdc021a29347976e4d0
Stepan Droniv <65362549+stdroniv@users.noreply.github.com>
2021-10-10 14:44:58 +0300

    Add validation CreateUserProfilePhoto unit tests

--- src/Application/Users/Commands/CreateUserProfilePhoto/CreateUserProfilePhotoCommandValidator.cs
+++ src/Application/Users/Commands/CreateUserProfilePhoto/CreateUserProfilePhotoCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,6 +11,16 @@ namespace BvAcademyPortal.Application.Users.Commands.CreateUserProfilePhoto
 {
     public class CreateUserProfilePhotoCommandValidator: AbstractValidator<CreateUserProfilePhotoCommand>
     {
+        public CreateUserProfilePhotoCommandValidator()
+        {
+            RuleFor(c => c.FormFile).NotNull();
+            RuleFor(c => c.FormFile).SetValidator(new UserFileValidator());
+            RuleFor(c => c.Details.BlobName).NotEmpty();
+        }
+    }
+
+    public class UserFileValidator : AbstractValidator<IFormFile>
+    {
         static readonly string[] _allowedExtensions = new string[]
         {
             ".jpeg",
@@ -19,12 +30,10 @@ namespace BvAcademyPortal.Application.Users.Commands.CreateUserProfilePhoto
         static readonly int _maxSize = 30 * 1_048_576;
         static readonly int _minSize = 1;
 
-        public CreateUserProfilePhotoCommandValidator()
+        public UserFileValidator()
         {
-            RuleFor(c => c.FormFile).NotNull();
-            RuleFor(c => c.FormFile.FileName).Must(n => _allowedExtensions.Contains(Path.GetExtension(n)));
-            RuleFor(c => c.FormFile.Length).InclusiveBetween(_minSize, _maxSize);
-            RuleFor(c => c.Details.BlobName).NotEmpty();
+            RuleFor(f => f.FileName).Must(fn => _allowedExtensions.Contains(Path.GetExtension(fn)));
+            RuleFor(f => f.Length).InclusiveBetween(_minSize, _maxSize);
         }
     }
 }
---
+++ tests/Application.IntegrationTests/Users/Commands/CreateUserProfilePhotoTest.cs
@@ -0,0 +1,81 @@
+﻿using BvAcademyPortal.Application.Common.Exceptions;
+using BvAcademyPortal.Application.Common.Models;
+using BvAcademyPortal.Application.Users.Commands.CreateUserProfilePhoto;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BvAcademyPortal.Application.IntegrationTests.Users.Commands
+{
+    public class CreateUserProfilePhotoTest
+    {
+        [TestCaseSource(nameof(ThrowValidationExceptionCases))]
+        public void ShouldThrowValidationException(CreateUserProfilePhotoCommand command)
+        {
+            FluentActions.Invoking(() =>
+                Testing.SendAsync(command)).Should().Throw<ValidationException>();
+        }
+
+        static IEnumerable<CreateUserProfilePhotoCommand> ThrowValidationExceptionCases()
+        {
+            yield return new CreateUserProfilePhotoCommand();
+            yield return new CreateUserProfilePhotoCommand()
+            {
+                FormFile = null,
+                Details = new ProfilePhotoDetails() { BlobName = "testname"}
+            };
+            yield return new CreateUserProfilePhotoCommand() 
+            { 
+                FormFile = new TestUserProfilePhoto() { FileName = "testname.jpg", Length = 5 * 1_048_576 }, 
+                Details = new ProfilePhotoDetails() { BlobName = "testname"}
+            };
+            yield return new CreateUserProfilePhotoCommand()
+            {
+                FormFile = new TestUserProfilePhoto() { FileName = "testname.jpeg", Length = 0 * 1_048_576 },
+                Details = new ProfilePhotoDetails() { BlobName = "testname" }
+            };
+            yield return new CreateUserProfilePhotoCommand()
+            {
+                FormFile = new TestUserProfilePhoto() { FileName = "testname.jepg", Length = 5 * 1_048_576 },
+                Details = new ProfilePhotoDetails() { BlobName = "" }
+            };
+        }
+
+        class TestUserProfilePhoto : IFormFile
+        {
+            public string ContentType => throw new NotImplementedException();
+
+            public string ContentDisposition => throw new NotImplementedException();
+
+            public IHeaderDictionary Headers => throw new NotImplementedException();
+
+            public long Length { get; set; }
+
+            public string Name => throw new NotImplementedException();
+
+            public string FileName { get; set; }
+
+            public void CopyTo(Stream target)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task CopyToAsync(Stream target, CancellationToken cancellationToken = default)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Stream OpenReadStream()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
