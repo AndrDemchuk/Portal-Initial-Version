c3f2f89bd4c97164613ee4044677c25a49463bea
Stepan Droniv <65362549+stdroniv@users.noreply.github.com>
2021-10-25 11:55:54 +0300

    Update migrations

--- src/Infrastructure/Persistence/Migrations/20211025085219_AddSoftDelete.Designer.cs
+++ src/Infrastructure/Persistence/Migrations/20211025085219_AddSoftDelete.Designer.cs
@@ -4,14 +4,16 @@ using BvAcademyPortal.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BvAcademyPortal.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211025085219_AddSoftDelete")]
+    partial class AddSoftDelete
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
